70a89cdd30b04871448e1cd5b75494d4f2138185
Jonathan Cavalheiro <jonathan.delima@hotmail.com>
2022-10-17 22:27:31 -0300

    initial commit

---
+++ src/test/java/br/com/jonathan/pizzariafuturodev/PizzariaFuturodevApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jonathan.pizzariafuturodev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzariaFuturodevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
